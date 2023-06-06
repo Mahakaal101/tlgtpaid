bb12006479b095b1442de4c65f54caa967b085cb
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-06-06 16:06:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -381,7 +381,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://shortnfly.in/api'
+    url = f'https://shortnfly.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
