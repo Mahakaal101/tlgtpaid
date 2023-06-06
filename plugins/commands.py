ee0c20d486f8a916499bc6026f7ebffc322b8410
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-06-06 17:32:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@GreyMattersYT') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('LATEST MOVIES HERE', url='https://t.me/+WOrSTTigB7s5YjY1') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
